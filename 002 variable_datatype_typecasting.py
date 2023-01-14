34e269afbb6f1c17273a5630f4114a11b1d295bf
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-14 19:34:59 +0530

    Create 002 variable_datatype_typecasting.py

---
+++ 002 variable_datatype_typecasting.py
@@ -0,0 +1,25 @@
+# var1="vaidu "
+# var2=36.7
+# var3=2
+# var4="ghelani"
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(var2+var3)
+# print(var1+var4)
+# var5="54"
+# var6="66"
+# print(var5+var6)
+# print(int(var5)+int(var6))
+# print(10*"This will print multiple time\n")
+# print(5*str(var3))
+# print("Enter your number")
+# inpnum=input()
+# print("You entered ",int(inpnum))
+# print("number icrement by 10 is ",int(inpnum)+10)
+
+print("Enter first number ")
+n1=input()
+print("Enter second")
+n2=input()
+print("Sum of numbers is ",int(n1)+int(n2))
