d6988fa45b1957fa87385e390d1c4ef9433fa0c6
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-17 20:40:22 +0530

    Create dictonary_ex.py

---
+++ dictonary_ex.py
@@ -0,0 +1,9 @@
+d1={"grenadine": "thin syrup made from pomegranate juice; used in mixed drinks",
+    "Demonic": "relating to or characteristic of demons or evil spirits",
+    "hefty": "Of considerable weight and size",
+    "concretize": "make something concrete",
+    "manikin": "a life-size dummy used to display clothes",
+    "tamp": "press down tightly"}
+print("Available words in dictonary are || 'grenadine' , 'Demonic', 'hefty', 'concretize', 'manikin', 'tamp'||")
+i=input("Enter the word to get the meaning\n")
+print(d1.get(i))
