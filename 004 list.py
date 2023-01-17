e1c4c88700d23128cc5fda0c65c68d8d46e46853
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-17 19:51:50 +0530

    Create 004 list.py

---
+++ 004 list.py
@@ -0,0 +1,48 @@
+#Mixed list(Mutable)
+# friends=["abhay","jaydeep","divyesh","dharmik","nirjal","darshan", 56,11,22,12]
+# print(friends)
+# print(friends[4])
+# numbers=[291,22,13,74,13,11,1,13,45,1056,] #Numbers list
+# # print(numbers)
+# # print(numbers[5])
+# # numbers.sort() #It will sort lit adn change in the orignal list
+# # print(numbers)
+#
+# numbers.append(71)  #Adds an element at the end of the list
+# print(numbers)
+#
+# print(numbers.copy())	    #Returns a copy of the list
+# print(numbers.count(13))	#Returns the number of elements with the specified value
+#
+# '''numbers.extend()	        #Add the elements of a list (or any iterable), to the end of the current list '''
+# print(numbers)
+#
+# print(numbers.index(11))	#Returns the index of the first element with the specified value
+#
+# numbers.insert(5,65)	    #Adds an element at the specified position
+# print(numbers)
+#
+# numbers.pop()               #Removes the element at the specified position
+# print(numbers)
+#
+# numbers.remove(22)	        #Removes the first item with the specified value
+# print(numbers)
+#
+# numbers.reverse()	        #Reverses the order of the list
+# print(numbers)
+#
+# numbers.sort()	            #Sorts the list
+# print(numbers)
+#
+# # numbers.clear()	        #Removes all the elements from the list
+# # print(numbers)
+#
+# #Slicing of list
+# # print(numbers[:])
+# # print(numbers[1:4])
+# # print(numbers[::2])
+# # print(numbers[::-1])
+
+tp=(11,22,12)
+print(tp)
+#tp[1]=45    #It will gives an error
