1b194c2fdc030052dd43d9994db4a33db6c9fd89
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-18 13:48:08 +0530

    Create E6 break&continue_ex.py

---
+++ E6 break&continue_ex.py
@@ -0,0 +1,8 @@
+while(True):
+    inp=int(input("Enter the number"))
+    if inp>100:
+        print("Congrats!! you have  entered a number greater than 100 ")
+        break
+    else:
+        print("Try again\n")
+        continue
