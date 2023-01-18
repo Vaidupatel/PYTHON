aef6d3ad77919bdc4ec6fd0ccd68fe8f66922033
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-18 12:20:00 +0530

    Create 007 if_else.py

---
+++ 007 if_else.py
@@ -0,0 +1,21 @@
+# var1=6
+# var2=56
+#
+# var3=int(input("Enter the value of var3"))
+# if var3>var2:
+#     print("Greter")
+# elif var3==var2:
+#     print("Equal")
+# else:
+#     print("Lesser")
+
+list=[1,2,3,4,5,]
+
+print(5 in list)
+
+if 5 in list:
+    print("Yes its in list")
+
+print(15 not in list)
+if 15 not in list:
+    print("No its not in list")
