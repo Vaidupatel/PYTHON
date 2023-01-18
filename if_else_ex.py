553577e1891a979751498a2d856641b8bf5baecb
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-18 12:20:37 +0530

    Create if_else_ex.py

---
+++ if_else_ex.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age"))
+if age>18:
+    print("Congrates!! You can drive")
+elif age<18:
+    print("Opps!! You cant drive")
+elif age==18:
+    print("We cant say any thing before your taste")
+else:
+    print("Enter correct age")
