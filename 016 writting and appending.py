ae3dae18ce7e5bda0f3430d63bd04751bd86f4c9
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-03-04 12:27:50 +0530

    Create 016 writting and appending.py

---
+++ 016 writting and appending.py
@@ -0,0 +1,19 @@
+# f = open("vaiduu.txt", "w")
+# a = f.write("Viadu will became great hacker of the history\n")
+# print(a)
+# f.close()
+
+# f = open("vaiduu.txt", "a")
+# a = f.write("Viadu will became great hacker of the history\n")
+# print(a)
+# f.close()
+
+# f = open("vaiduu.txt", "w")
+# a = f.write("Viadu will became great hacker of the history\n")
+# print(a)
+# f.close()
+
+#handle read and write both
+f = open("vaiduu.txt", "r+")
+print(f.read())
+f.write("As soon as")
