6a340bd56eb359319f6bea7ed27e656eff7a9f10
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-03-13 19:10:48 +0530

    Create 024 join_function.py

---
+++ 024 join_function.py
@@ -0,0 +1,8 @@
+lis = ["vaid", "nirjal", "jay", "deeo", "darshan", "prince", "sahil"]
+
+# for i in lis:
+#     print(i,"and",end=" ")
+
+a = " and ".join(lis)
+print(a)
+print("This is my best friendss")
