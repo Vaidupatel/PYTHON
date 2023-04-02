ff7632b65b6fec3fe6c8fec6a19e69b3787987b2
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-04-02 10:07:34 +0530

    Create 029 oops3.py

---
+++ 029 oops3.py
@@ -0,0 +1,25 @@
+class Employee:
+   no_of_leaves = 8
+   def __init__(self, aname, asalary, arole):
+       self.name = aname
+       self.salary = asalary
+       self.role = arole
+
+
+    # def printdetails(self):
+    #     return f"Name is {self.name}. Salary is {self.sallary}. and role is {self.role}"
+
+
+vaid = Employee('vaid', 45000, 'instructor')
+# nirjal = Employee()
+
+# These are the instance but these are not the property of class
+# vaid.name = 'vaid'
+# vaid.sallary = 45000
+# vaid.role = 'stdent'
+# nirjal.name = 'nirjal'
+# nirjal.sallary = 50000
+# nirjal.role = 'instructor'
+
+print(vaid.salary)
+# print(nirjal.printdetails())
