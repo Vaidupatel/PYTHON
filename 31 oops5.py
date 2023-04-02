a24d85a077c38ea3f1db0c41552ecf8a8fc60456
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-04-02 10:37:08 +0530

    Create 31 oops5.py

---
+++ 31 oops5.py
@@ -0,0 +1,18 @@
+class Eployee:
+    no_of_leaves = 8
+    def __init__(self, anme, aslary, arole):
+        self.name = anme
+        self.slaray = aslary
+        self.role = arole
+
+
+    @classmethod
+    def from_dash(cls, string):
+        # para = string.split("-")
+        # print(para)
+        # return cls(para[0], para[1], para[2])
+        return cls(*string.split("-"))
+
+prince = Eployee.from_dash("prince-47000-view")
+print(prince.slaray)
+print(prince.no_of_leaves)
